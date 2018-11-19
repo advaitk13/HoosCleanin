62a9c96ab3647c6aaae740c31c79bb22e85a83d1
emurdock1101 <emurdock1101@gmail.com>
2018-11-19 12:36:38 -0500

    more member.html and jobs.js stuff

--- jobs.js
+++ jobs.js
@@ -7,20 +7,29 @@
         this.price = price; 
     }
 
+    P_job.prototype.toString = function() {
+    return "Title: " + this.post_title + " --- Date: " + this.date 
+	+ " --- Address: " + this.location + " --- Price: $" + this.price + "";
+    };
+
     function V_job(date, post_title, location, price) {
         this.date = randomDate(new Date(2018, 0, 1), new Date()); 
         this.post_title = post_title; 
         this.location = location; 
         this.price = price; 
     }
+	
+	 V_job.prototype.toString = function() {
+    return "Title: " + this.post_title + " --- Date: " + this.date 
+	+ " --- Address: " + this.location + " --- Price: $" + this.price + "";
+    };
 
                                     
     var p_job1 =  new P_job(randomDate(new Date(2018, 0, 1), new Date()), "Deck cleaning", "1303 Wertland Street A6", "20");
-
     var p_job2 =  new P_job(randomDate(new Date(2018, 0, 1), new Date()), "whole apartment", "1303 Wertland Street A6", "50");
+	var p_job3 =  new P_job(randomDate(new Date(2018, 0, 1), new Date()), "kitchen, bathrooms, bedrooms", "1303 Wertland Street A6", "40");
     
-    var p_jobs_strings = new Array(JSON.stringify(p_job1),JSON.stringify(p_job2) );
-    
+    var posts = new Array(p_job1.toString(), p_job2.toString(), p_job3 );
 
 
     var v_job1 = new V_job (randomDate(new Date(2018, 0, 1), new Date()), "All house bathrooms", "66 Jefferson Park Avenue", "0");
@@ -31,13 +40,13 @@
     var v_job6 = new V_job (randomDate(new Date(2018, 0, 1), new Date()), "Basic apartment cleaning", "560 14th Street", "30");
 
 
-    var v_jobs_strings = new Array();
-    v_jobs_strings.push(JSON.stringify(v_job1));
-    v_jobs_strings.push(JSON.stringify(v_job2));
-    v_jobs_strings.push(JSON.stringify(v_job3));
-    v_jobs_strings.push(JSON.stringify(v_job4));
-    v_jobs_strings.push(JSON.stringify(v_job5));
-    v_jobs_strings.push(JSON.stringify(v_job6));
+    var views = new Array();
+    views.push(v_job1.toString());
+    views.push(v_job2.toString());
+    views.push(v_job3.toString());
+    views.push(v_job4.toString());
+    views.push(v_job5.toString());
+    views.push(v_job6.toString());
 
 //var random = Math.random() * (+max - +min) + +min; 
 
@@ -45,28 +54,9 @@ function randomDate(start, end) {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 }
 
-function getYourPosts() {
-    var posts = new Array();
-    for (var i =0; i < (Math.random() * 1) + 1; i++)
-    {
-        posts.push(p_jobs_strings[i]);
-    }
-    return posts;
-}
-
-function getYourViews() {
-    var views = new Array();
-    for (var i =0; i < (Math.random() * 3) + 2; i++)
-    {
-        views.push(v_jobs_strings[i]);
-    }
-    return views;
-}
-
 
 
 function makeUL1() {
-    var posts = getYourPosts();
     var ul = document.createElement('ul');
     document.getElementById('demo1').appendChild(ul);
 
@@ -78,7 +68,6 @@ function makeUL1() {
 }
 
 function makeUL2() {
-    var views = getYourViews();
     var ul = document.createElement('ul');
     document.getElementById('demo2').appendChild(ul);
 
